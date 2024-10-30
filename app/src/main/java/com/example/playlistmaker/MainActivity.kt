796d563459dd0d212a500f81de03372d29d14bfe
AlexeyNenashev <en169@yandex.ru>
2024-10-30 23:03:20 +0700

    Домашнее задание спринта № 8

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button1 = findViewById<Button>(R.id.button_search)
+        //val imageClickListener1: View.OnClickListener = object : View.OnClickListener {
+        //override fun onClick(v: View?) {
+        //Toast.makeText(this@MainActivity, "Нажали кнопку Поиск", Toast.LENGTH_SHORT).show()
+        //}
+        //}
+        //button1.setOnClickListener(imageClickListener1)
+        button1.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
+        val button2 = findViewById<Button>(R.id.button_library)
+        button2.setOnClickListener {
+            val displayIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        val button3 = findViewById<Button>(R.id.button_settings)
+        button3.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
     }
 }
