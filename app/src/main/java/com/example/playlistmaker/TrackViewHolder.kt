a54718bce2010b24cf2326e7dae18f24df3ea986
AlexeyNenashev <en169@yandex.ru>
2024-11-25 12:16:09 +0700

    Спринт № 10, учёл замечания после ревью

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -10,14 +11,16 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
-    //private val rootLayout: LinearLayout = itemView.findViewById(R.id.rootLayout)
     private val picture: ImageView = itemView.findViewById(R.id.picture)
     private val trackName: TextView = itemView.findViewById(R.id.trackname)
     private val author: TextView = itemView.findViewById(R.id.author)
     private val time: TextView = itemView.findViewById(R.id.time)
+    private val radius: Int = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        2.0F,
+        itemView.resources.displayMetrics).toInt()
 
     fun bind(item: Track) {
-        //picture.setImageResource(R.drawable.placeholder)
         trackName.text = item.trackName
         author.text = item.artistName
         time.text = item.trackTime
@@ -25,7 +28,7 @@ class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
             .load(item.artworkUrl100)
             .placeholder(R.drawable.placeholder)
             .fitCenter()
-            .transform(RoundedCorners(10))
+            .transform(RoundedCorners(radius))
             .into(picture)
     }
 }
