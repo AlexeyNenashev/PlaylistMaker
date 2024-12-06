84323ea54ce072a5dedccaa5a00c1e1886527fd4
AlexeyNenashev <en169@yandex.ru>
2024-12-07 00:51:25 +0700

    Спринт № 11, сделал поиск в iTunes

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -15,10 +14,25 @@ import androidx.core.view.WindowInsetsCompat
 import android.view.inputmethod.InputMethodManager
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
+    private val tracks = ArrayList<Track>()
     private var searchValue = ""
+
+    private val iTunesBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(ItunesApi::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +52,17 @@ class SearchActivity : AppCompatActivity() {
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
+        val trackAdapter = TrackAdapter(tracks)
+
+        val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
+        rvTrack.adapter = trackAdapter
+
         clearButton.setOnClickListener {
             inputEditText.setText("")
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+            tracks.clear()
+            trackAdapter.notifyDataSetChanged()
         }
 
         inputEditText.doOnTextChanged { s, start, before, count ->
@@ -49,43 +70,48 @@ class SearchActivity : AppCompatActivity() {
             searchValue = s.toString()
         }
 
-        val trackAdapter = TrackAdapter(
-            listOf(
-                Track(
-                    "Smells Like Teen Spirit",
-                    "Nirvana",
-                    "5:01",
-                    "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    "Billie Jean",
-                    "Michael Jackson",
-                    "4:35",
-                    "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    "Stayin' Alive",
-                    "Bee Gees",
-                    "4:10",
-                    "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    "Whole Lotta Love",
-                    "Led Zeppelin",
-                    "5:33",
-                    "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    "Sweet Child O'Mine",
-                    "Guns N' Roses",
-                    "5:03",
-                    "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-                ),
-            )
-        )
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (searchValue.isNotEmpty()) {
+                    iTunesService.search(searchValue).enqueue(object :
+                        Callback<TrackResponse> {
+                        override fun onResponse(call: Call<TrackResponse>,
+                                                response: Response<TrackResponse>
+                        ) {
+                            if (response.code() == 200) {
+                                tracks.clear()
+                                if (response.body()?.results?.isNotEmpty() == true) {
+                                    Log.d("TestLogTag", "$searchValue: something found")
+                                    tracks.addAll(response.body()?.results ?: emptyList<Track>())
+                                    Log.d("TestLogTag", "$searchValue: ${tracks.size} found")
+                                }
+                                trackAdapter.notifyDataSetChanged()
+                                if (tracks.isEmpty()) {
+                                    //showMessage(getString(R.string.nothing_found), "")
+                                    Log.d("TestLogTag", "$searchValue: nothing found")
+                                } else {
+                                    //showMessage("", "")
+                                    Log.d("TestLogTag", "$searchValue: ok")
+                                }
+                            } else {
+                                //showMessage(getString(R.string.something_went_wrong), response.code().toString())
+                                Log.d("TestLogTag", "$searchValue: something went wrong, ${response.code()}")
+                            }
+                        }
 
-        val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
-        rvTrack.adapter = trackAdapter
+                        override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                            //showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                            Log.d("TestLogTag", "$searchValue: something went wrong, ${t.message}")
+                        }
+
+                    })
+                }
+                Log.d("TestLogTag", searchValue)
+                //true
+            }
+            //false
+            actionId == EditorInfo.IME_ACTION_DONE
+        }
 
     }
 
