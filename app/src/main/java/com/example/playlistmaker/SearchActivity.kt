b96183f6cacb6b12e25603b17f251a292b9f7fa3
AlexeyNenashev <en169@yandex.ru>
2024-12-09 13:04:19 +0700

    Спринт № 11, добавил сообщения об ошибках

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -12,6 +12,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+
+import android.widget.TextView
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -23,7 +26,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 class SearchActivity : AppCompatActivity() {
 
     private val tracks = ArrayList<Track>()
+    private val trackAdapter = TrackAdapter(tracks)
     private var searchValue = ""
+    private var messageShown = false
 
     private val iTunesBaseUrl = "https://itunes.apple.com"
 
@@ -51,8 +56,8 @@ class SearchActivity : AppCompatActivity() {
 
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val refreshButton = findViewById<Button>(R.id.messageButton)
 
-        val trackAdapter = TrackAdapter(tracks)
 
         val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
         rvTrack.adapter = trackAdapter
@@ -63,6 +68,12 @@ class SearchActivity : AppCompatActivity() {
             inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
             tracks.clear()
             trackAdapter.notifyDataSetChanged()
+            showOrHideMessage(Msg.HIDE)
+        }
+
+        refreshButton.setOnClickListener {
+            showOrHideMessage(Msg.HIDE)
+            makeSearch()
         }
 
         inputEditText.doOnTextChanged { s, start, before, count ->
@@ -72,40 +83,7 @@ class SearchActivity : AppCompatActivity() {
 
         inputEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                if (searchValue.isNotEmpty()) {
-                    iTunesService.search(searchValue).enqueue(object :
-                        Callback<TrackResponse> {
-                        override fun onResponse(call: Call<TrackResponse>,
-                                                response: Response<TrackResponse>
-                        ) {
-                            if (response.code() == 200) {
-                                tracks.clear()
-                                if (response.body()?.results?.isNotEmpty() == true) {
-                                    Log.d("TestLogTag", "$searchValue: something found")
-                                    tracks.addAll(response.body()?.results ?: emptyList<Track>())
-                                    Log.d("TestLogTag", "$searchValue: ${tracks.size} found")
-                                }
-                                trackAdapter.notifyDataSetChanged()
-                                if (tracks.isEmpty()) {
-                                    //showMessage(getString(R.string.nothing_found), "")
-                                    Log.d("TestLogTag", "$searchValue: nothing found")
-                                } else {
-                                    //showMessage("", "")
-                                    Log.d("TestLogTag", "$searchValue: ok")
-                                }
-                            } else {
-                                //showMessage(getString(R.string.something_went_wrong), response.code().toString())
-                                Log.d("TestLogTag", "$searchValue: something went wrong, ${response.code()}")
-                            }
-                        }
-
-                        override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
-                            //showMessage(getString(R.string.something_went_wrong), t.message.toString())
-                            Log.d("TestLogTag", "$searchValue: something went wrong, ${t.message}")
-                        }
-
-                    })
-                }
+                makeSearch()
                 Log.d("TestLogTag", searchValue)
                 //true
             }
@@ -128,6 +106,79 @@ class SearchActivity : AppCompatActivity() {
         inputEditText.setText(searchValue)
     }
 
+    private fun makeSearch() {
+        if (searchValue.isNotEmpty()) {
+            iTunesService.search(searchValue).enqueue(object :
+                Callback<TrackResponse> {
+                override fun onResponse(call: Call<TrackResponse>,
+                                        response: Response<TrackResponse>
+                ) {
+                    if (response.code() == 200) {
+                        tracks.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            Log.d("TestLogTag", "$searchValue: something found")
+                            tracks.addAll(response.body()?.results ?: emptyList<Track>())
+                            Log.d("TestLogTag", "$searchValue: ${tracks.size} found")
+                        }
+                        trackAdapter.notifyDataSetChanged()
+                        if (tracks.isEmpty()) {
+                            showOrHideMessage(Msg.NOTHING_FOUND)
+                            //showMessage(getString(R.string.nothing_found), "")
+                            Log.d("TestLogTag", "$searchValue: nothing found")
+                        } else {
+                            showOrHideMessage(Msg.HIDE)
+                            //showMessage("", "")
+                            Log.d("TestLogTag", "$searchValue: ok")
+                        }
+                    } else {
+                        showOrHideMessage(Msg.SOMETHING_WRONG)
+                        //showMessage(getString(R.string.something_went_wrong), response.code().toString())
+                        Log.d("TestLogTag", "$searchValue: something went wrong, ${response.code()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    showOrHideMessage(Msg.SOMETHING_WRONG)
+                    //showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                    Log.d("TestLogTag", "$searchValue: something went wrong, ${t.message}")
+                }
+
+            })
+        }
+    }
+
+    private enum class Msg{
+        NOTHING_FOUND,
+        SOMETHING_WRONG,
+        HIDE;
+    }
+
+    private fun showOrHideMessage(msg: Msg) {
+        val layout = findViewById<View>(R.id.messageLayout)
+        val icon = findViewById<ImageView>(R.id.messageIcon)
+        val text = findViewById<TextView>(R.id.messageText)
+        val button = findViewById<Button>(R.id.messageButton)
+        when(msg) {
+            Msg.NOTHING_FOUND -> {
+                icon.setImageDrawable(getDrawable(R.drawable.nothing_found))
+                text.text = getString(R.string.message_nothing_found)
+                button.visibility = View.GONE
+                layout.visibility = View.VISIBLE
+            }
+            Msg.SOMETHING_WRONG -> {
+                icon.setImageDrawable(getDrawable(R.drawable.something_wrong))
+                text.text = getString(R.string.message_something_wrong)
+                button.visibility = View.VISIBLE
+                layout.visibility = View.VISIBLE
+            }
+            Msg.HIDE -> {
+                if (messageShown)
+                    layout.visibility = View.GONE
+            }
+        }
+        messageShown = (msg != Msg.HIDE)
+    }
+
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
@@ -136,7 +187,7 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-    companion object {
+    private companion object {
         const val SEARCH_VALUE = "SEARCH_VALUE"
     }
 
