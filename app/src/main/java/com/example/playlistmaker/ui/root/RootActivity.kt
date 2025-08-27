d44ae3ce88f65973d94558f417b440411c1b8507
AlexeyNenashev <en169@yandex.ru>
2025-08-27 22:21:00 +0700

    Спринт № 19, прикрутил BottomNavigationView

--- app/src/main/java/com/example/playlistmaker/ui/root/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/root/RootActivity.kt
@@ -3,6 +3,8 @@ package com.example.playlistmaker.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityRootBinding
 import com.example.playlistmaker.ui.main.activity.MainFragment
@@ -17,11 +19,10 @@ class RootActivity : AppCompatActivity() {
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //if (savedInstanceState == null) {
-        //    supportFragmentManager.commit {
-        //        this.add(R.id.rootFragmentContainerView, MainFragment())
-        //    }
-        //}
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 
 }
