50ef2e204bc20ff80d1d69518b36c01f60b7f513
AlexeyNenashev <en169@yandex.ru>
2025-10-16 11:40:13 +0700

    Спринт № 21, сделал слой UI, не работает

--- app/src/main/java/com/example/playlistmaker/ui/library/activity/SelectedTracksFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/library/activity/SelectedTracksFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentSelectedTracksBinding
 import com.example.playlistmaker.domain.model.Track
 import com.example.playlistmaker.ui.library.SelectedTracksState
 import com.example.playlistmaker.ui.library.view_model.SelectedTracksViewModel
+import com.example.playlistmaker.ui.player.activity.PlayerFragment
+import com.example.playlistmaker.ui.search.activity.TrackAdapter
+import com.example.playlistmaker.utils.debounce
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SelectedTracksFragment : Fragment() {
+
+    private lateinit var onClickDebounce: (Track) -> Unit
+    private val trackAdapter = TrackAdapter { onClickDebounce(it) }
+
     private var _binding: FragmentSelectedTracksBinding? = null
     private val binding get() = _binding!!
     private val viewModel: SelectedTracksViewModel by viewModel()
@@ -34,6 +44,12 @@ class SelectedTracksFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.rvTrack.adapter = trackAdapter
+
+        onClickDebounce = debounce<Track>(CLICK_DEBOUNCE_DELAY, viewLifecycleOwner.lifecycleScope, false) { track ->
+            launchPlayerScreen(track)
+        }
+
         viewModel.observeState().observe(viewLifecycleOwner) {
             when (it) {
                 is SelectedTracksState.NoTracks -> showNoTracks()
@@ -43,15 +59,27 @@ class SelectedTracksFragment : Fragment() {
     }
 
     private fun showNoTracks() {
+        binding.rvTrack.visibility = View.GONE
         binding.messageLayout.visibility = View.VISIBLE
     }
 
     private fun showTracks(tracks: List<Track>) {
+        binding.messageLayout.visibility = View.GONE
+        trackAdapter.tracks.clear()
+        trackAdapter.tracks.addAll(tracks)
+        trackAdapter.notifyDataSetChanged()
         binding.messageLayout.visibility = View.VISIBLE
     }
 
+    fun launchPlayerScreen(t: Track) {
+        findNavController().navigate(
+            R.id.action_selectedTracksFragment_to_playerFragment,
+            PlayerFragment.createArgs(t)
+        )
+    }
 
     companion object {
         fun newInstance() = SelectedTracksFragment()
+        private const val CLICK_DEBOUNCE_DELAY = 300L
     }
 }
